43af8c958337fe0dfc8dd8664dba7bfc7e7fd634
redboostik <redboost8@gmail.com>
2021-11-01 09:20:11 +0300

    lab 5

---
+++ IGI_Web_953501_Smolyar/IGI_Web_953501_Smolyar/Entities/Drink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IGI_Web_953501_Smolyar.Entities
+{
+    public class Drink
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Cost { get; set; } 
+        public string Image { get; set; } 
+        public int GroupId { get; set; }
+        public DrinkGroup Group { get; set; }
+    }
+}
---
+++ IGI_Web_953501_Smolyar/IGI_Web_953501_Smolyar/Entities/DrinkGroup.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace IGI_Web_953501_Smolyar.Entities
+{
+    public class DrinkGroup
+    {
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public List<Drink> Drinks { get; set; }
+    }
+}
